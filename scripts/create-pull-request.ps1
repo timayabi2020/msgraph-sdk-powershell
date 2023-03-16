7b1e08dc44f0d521aec49e5c1b6a3d3f61916020
Timothy Wamalwa <timwamalwa@gmail.com>
2023-03-16 11:39:11 +0300

    Troubleshooting

--- scripts/create-pull-request.ps1
+++ scripts/create-pull-request.ps1
@@ -9,7 +9,7 @@ if (![string]::IsNullOrEmpty($env:BaseBranch))
 {
     $baseBranchParameter = "-B $env:BaseBranch" # optionally pass the base branch if provided as the PR will target the default branch otherwise
 }
-
+Write-Host "Base branch => " $env:BaseBranch
 # Code owners will be added automatically as reviewers.
 Invoke-Expression "gh auth login" # login to GitHub
 Invoke-Expression "gh pr create -t ""$title"" -b ""$body"" $baseBranchParameter | Write-Host"
